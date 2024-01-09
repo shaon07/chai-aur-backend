da40216d9b322df5eade182848b8786cddcca193
Shaon07 <alishaon77@gmail.com>
2024-01-09 23:26:24 +0600

    [add] middlewares

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
-import express from 'express';
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
 
 const app = express();
 
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.json({ limit: process.env.LIMIT }));
+app.use(express.urlencoded({ extended: true, limit: process.env.LIMIT }));
+
 export { app };
+
