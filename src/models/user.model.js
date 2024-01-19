f6ff2b254777539bb527283a26d5809f7ae8a501
Shaon07 <alishaon77@gmail.com>
2024-01-19 23:27:18 +0600

    [add] jsonwebtoken and password hash in user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,3 +1,5 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema(
@@ -47,5 +49,43 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", function(){
+  if(this.isModified("password")){
+    this.password = bcrypt.hash(this.password, 10);
+  }
+
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function(){
+  return await bcrypt.compare(this.password, this.password);
+}
+
+userSchema.methods.generateAccessToken = async function(){
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN,
+    }
+  )
+}
+
+userSchema.methods.generateRefreshToken = async function(){
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN,
+    }
+  )
+}
 
 export default User = mongoose.model("User", userSchema);
